8ad6f809eb6ef3ee1ca3e0378a433da0ebe6da67
goppd <goppdima@gmail.com>
2025-12-11 21:48:58 +0100

    Commited new  project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+import CalculatorForm from './components/calculatorForm'
+import { Container, TextField, Button, Typography } from '@mui/material'
+
+function App() {
+  return <></>
+}
+
+export default App
---
+++ src/components/calculatorForm/index.jsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.css'
+
+function CalculatorForm() {
+  return <CalculatorForm />
+}
+
+export default CalculatorForm
