b2f47718981498610db06c5633a79eb5085ac22f
goppd <goppdima@gmail.com>
2025-12-14 19:59:27 +0100

    Added handleAdd, handleSubtract, handleMultiply, handleDivide and Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css'
 import CalculatorForm from './components/calculatorForm'
-import { Container, TextField, Button, Typography } from '@mui/material'
 
 function App() {
-  return <></>
+  return <CalculatorForm />
 }
 
 export default App
--- src/components/calculatorForm/index.jsx
+++ src/components/calculatorForm/index.jsx
@@ -1,7 +1,41 @@
 import styles from './styles.module.css'
+import { useState } from 'react'
+import { Container, TextField, Button, Typography } from '@mui/material'
 
 function CalculatorForm() {
-  return <CalculatorForm />
+  const [num1, setNum1] = useState('')
+  const [num2, setNum2] = useState('')
+  const [result, setResult] = useState(null)
+
+  const toNumber = (value) => Number(value)
+
+  const handleAdd = () => {
+    setResult(toNumber(num1) + toNumber(num2))
+  }
+
+  const handleSubtract = () => {
+    setResult(toNumber(num1) - toNumber(num2))
+  }
+
+  const handleMultiply = () => {
+    setResult(toNumber(num1) * toNumber(num2))
+  }
+
+  const handleDivide = () => {
+    if (toNumber(num2) === 0) {
+      setResult('Error: Division by zero')
+    } else {
+      setResult(toNumber(num1) / toNumber(num2))
+    }
+  }
+
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h4" gutterBottom align="center">
+        Калькулятор
+      </Typography>
+    </Container>
+  )
 }
 
 export default CalculatorForm
