b3dd67fb61bf7d9498d428eca1f26fb637758380
goppd <goppdima@gmail.com>
2025-12-14 20:18:09 +0100

    Added TextField, Box and Paper

--- src/components/calculatorForm/index.jsx
+++ src/components/calculatorForm/index.jsx
@@ -1,6 +1,12 @@
-import styles from './styles.module.css'
 import { useState } from 'react'
-import { Container, TextField, Button, Typography } from '@mui/material'
+import {
+  Container,
+  TextField,
+  Button,
+  Typography,
+  Box,
+  Paper,
+} from '@mui/material'
 
 function CalculatorForm() {
   const [num1, setNum1] = useState('')
@@ -30,10 +36,80 @@ function CalculatorForm() {
   }
 
   return (
-    <Container maxWidth="sm">
-      <Typography variant="h4" gutterBottom align="center">
-        Калькулятор
-      </Typography>
+    <Container maxWidth="sm" sx={{ mt: 4 }}>
+      <Box>
+        <Paper elevation={3} style={{ padding: 16 }}>
+          <Typography variant="h4" gutterBottom align="center">
+            Калькулятор
+          </Typography>
+
+          <TextField
+            label="Число 1"
+            variant="outlined"
+            fullWidth
+            type="number"
+            margin="normal"
+            value={num1}
+            onChange={(e) => setNum1(e.target.value)}
+          />
+
+          <TextField
+            label="Число 2"
+            variant="outlined"
+            fullWidth
+            type="number"
+            margin="normal"
+            value={num2}
+            onChange={(e) => setNum2(e.target.value)}
+          />
+
+          <Button
+            variant="contained"
+            fullWidth
+            color="primary"
+            onClick={handleAdd}
+            sx={{ mt: 2 }}
+          >
+            Сложение
+          </Button>
+
+          <Button
+            variant="contained"
+            fullWidth
+            color="primary"
+            onClick={handleSubtract}
+            sx={{ mt: 2 }}
+          >
+            Вычитание
+          </Button>
+
+          <Button
+            variant="contained"
+            fullWidth
+            color="primary"
+            onClick={handleMultiply}
+            sx={{ mt: 2 }}
+          >
+            Умножение
+          </Button>
+
+          <Button
+            variant="contained"
+            fullWidth
+            color="primary"
+            onClick={handleDivide}
+            sx={{ mt: 2 }}
+          >
+            Деление
+          </Button>
+
+          {result !== null && (
+            <Typography variant="h6" align="center" sx={{ mt: 4 }}>
+              Результат: {result}
+            </Typography>
+          )}
+        </Paper>
+      </Box>
     </Container>
   )
 }
